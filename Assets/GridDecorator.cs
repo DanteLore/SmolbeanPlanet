cda44a061328cfee73821e9cca161b443f37f3b4
DanteLore <dan.taylor@live.co.uk>
2023-03-24 16:38:32 +0000

    Infinite scrolling.  woop woop

--- Assets/GridDecorator.cs
+++ Assets/GridDecorator.cs
@@ -87,7 +87,7 @@ public class Chunk
 
         public MapSquare this[int x, int y]
         {
-            get { return map[(y * width) + x]; }
+            get { return map[((y) * width) + x]; }
         }
 
         public MapIndexer(MapSquare[] map, int width)
@@ -109,15 +109,15 @@ public class Chunk
         this.height = height;
         this.neighbourSpecs = neighbourSpecs;
         this.tileSpecs = tileSpecs;
-        this.map = new MapSquare[width * height];
-        this.mapIndex = new MapIndexer(map, width);
+        this.map = new MapSquare[this.width * this.height];
+        this.mapIndex = new MapIndexer(map, this.width);
 
         // Initialise the map with all possible combinations of tile
-        for (int x = 0; x < width; x++)
+        for (int x = 0; x < this.width; x++)
         {
-            for (int y = 0; y < height; y++)
+            for (int y = 0; y < this.height; y++)
             {
-                map[(y * width) + x] = new MapSquare(x, y, neighbourSpecs.Select(n => n.Index));
+                map[(y * this.width) + x] = new MapSquare(x, y, neighbourSpecs.Select(n => n.Index));
             }
         }
     }
@@ -213,6 +213,54 @@ public class Chunk
 
         recurse.ForEach(s => Collapse(map, s));
     }
+
+    internal void ProcessNeighbourChunkOnRight(Chunk rightNeighbour)
+    {
+        for(int i = 0; i < height; i++)
+        {
+            int tile = rightNeighbour.Map[0, i].TileIndex;
+
+            var target = Map[width - 1, i];
+            var allowed = neighbourSpecs.First(s => s.Index == tile).AllowedLeft;
+            target.Restrict(allowed);
+        }
+    }
+
+    internal void ProcessNeighbourChunkOnLeft(Chunk leftNeighbour)
+    {
+        for(int i = 0; i < height; i++)
+        {
+            int tile = leftNeighbour.Map[width - 1, i].TileIndex;
+
+            var target = Map[0, i];
+            var allowed = neighbourSpecs.First(s => s.Index == tile).AllowedRight;
+            target.Restrict(allowed);
+        }
+    }
+
+    internal void ProcessNeighbourChunkOnTop(Chunk topNeighbour)
+    {
+        for(int i = 0; i < width; i++)
+        {
+            int tile = topNeighbour.Map[i, 0].TileIndex;
+
+            var target = Map[i, height - 1];
+            var allowed = neighbourSpecs.First(s => s.Index == tile).AllowedBelow;
+            target.Restrict(allowed);
+        }
+    }
+
+    internal void ProcessNeighbourChunkOnBottom(Chunk bottomNeighbour)
+    {
+        for(int i = 0; i < width; i++)
+        {
+            int tile = bottomNeighbour.Map[i, height - 1].TileIndex;
+
+            var target = Map[i, 0];
+            var allowed = neighbourSpecs.First(s => s.Index == tile).AllowedAbove;
+            target.Restrict(allowed);
+        }
+    }
 }
 
 public class GridDecorator : MonoBehaviour
@@ -234,7 +282,6 @@ public class GridDecorator : MonoBehaviour
 
     private Dictionary<Vector2Int, Chunk> chunks = new Dictionary<Vector2Int, Chunk>();
 
-    // Start is called before the first frame update
     void Start()
     {
     }
@@ -244,34 +291,36 @@ public class GridDecorator : MonoBehaviour
         return a < 0 ? (a - b) / b : a / b;
     }
 
-    // Update is called once per frame
     void Update()
     {
         var pos = baseTilemap.WorldToCell(player.transform.position);
 
         var currentTile = new Vector2Int(DivTowardsNegInfinity((int)pos.x, chunkWidth) * chunkWidth, DivTowardsNegInfinity((int)pos.y, chunkHeight) * chunkHeight);
+        Vector2Int[] vectors = GetChunksToDraw(currentTile);
 
-        var vectors = new Vector2Int[] 
-        { 
-            currentTile, // This tile
-            new Vector2Int(currentTile.x - chunkWidth, currentTile.y), // Left
-            new Vector2Int(currentTile.x - chunkWidth, currentTile.y - chunkHeight), // Bottom Left
-            new Vector2Int(currentTile.x, currentTile.y - chunkHeight), // Bottom
-            new Vector2Int(currentTile.x + chunkWidth, currentTile.y - chunkHeight), // Bottom Right
-            new Vector2Int(currentTile.x + chunkWidth, currentTile.y), // Right
-            new Vector2Int(currentTile.x + chunkWidth, currentTile.y + chunkHeight), // Top Right
-            new Vector2Int(currentTile.x, currentTile.y + chunkHeight), // Top
-            new Vector2Int(currentTile.x - chunkWidth, currentTile.y + chunkHeight), // Top Left
-        };
-
-        foreach(Vector2Int vector in vectors)
+        foreach (Vector2Int vector in vectors)
         {
-            if(!chunks.ContainsKey(vector))
+            if (!chunks.ContainsKey(vector))
             {
                 Chunk chunk = new Chunk(chunkWidth, chunkHeight, neighbourSpecs, tileSpecs);
                 chunks.Add(vector, chunk);
 
-                // Copy edges from existing neighbour
+                // Copy edges from existing neighbours
+                var rightLocation = new Vector2Int(vector.x + chunkWidth,  vector.y);
+                if(chunks.ContainsKey(rightLocation))
+                    chunk.ProcessNeighbourChunkOnRight(chunks[rightLocation]);
+
+                var leftLocation = new Vector2Int(vector.x - chunkWidth,  vector.y);
+                if(chunks.ContainsKey(leftLocation))
+                    chunk.ProcessNeighbourChunkOnLeft(chunks[leftLocation]);
+
+                var topLocation = new Vector2Int(vector.x,  vector.y + chunkHeight);
+                if(chunks.ContainsKey(topLocation))
+                    chunk.ProcessNeighbourChunkOnTop(chunks[topLocation]);
+
+                var bottomLocation = new Vector2Int(vector.x,  vector.y - chunkHeight);
+                if(chunks.ContainsKey(bottomLocation))
+                    chunk.ProcessNeighbourChunkOnBottom(chunks[bottomLocation]);
 
                 var t = Time.realtimeSinceStartup;
                 chunk.BuildTilemap();
@@ -280,6 +329,22 @@ public class GridDecorator : MonoBehaviour
                 DrawChunk(chunk, vector);
             }
         }
+    }
+
+    private Vector2Int[] GetChunksToDraw(Vector2Int currentTile)
+    {
+        return new Vector2Int[]
+            {
+                currentTile, // This tile
+                new Vector2Int(currentTile.x - chunkWidth, currentTile.y), // Left
+                new Vector2Int(currentTile.x - chunkWidth, currentTile.y - chunkHeight), // Bottom Left
+                new Vector2Int(currentTile.x, currentTile.y - chunkHeight), // Bottom
+                new Vector2Int(currentTile.x + chunkWidth, currentTile.y - chunkHeight), // Bottom Right
+                new Vector2Int(currentTile.x + chunkWidth, currentTile.y), // Right
+                new Vector2Int(currentTile.x + chunkWidth, currentTile.y + chunkHeight), // Top Right
+                new Vector2Int(currentTile.x, currentTile.y + chunkHeight), // Top
+                new Vector2Int(currentTile.x - chunkWidth, currentTile.y + chunkHeight), // Top Left
+            };
     }
 
     private void DrawChunk(Chunk chunk, Vector2Int origin)
