29668dc532230c145785dd26454b726e2c8ec79b
DanteLore <dan.taylor@live.co.uk>
2023-03-24 13:28:00 +0000

    Bit of gentle refactoring

--- Assets/GridDecorator.cs
+++ Assets/GridDecorator.cs
@@ -73,11 +73,145 @@ public class MapSquare
 
 public class Chunk
 {
-    public MapSquare[] map;
+    private MapSquare[] map;
+    private int height;
+    private int width;
+    NeighbourSpec[] neighbourSpecs;
+    TileSpec[] tileSpecs;
+    System.Random r = new System.Random();
 
-    public Chunk(MapSquare[] map)
+    public class MapIndexer
     {
-        this.map = map;
+        private MapSquare[] map;
+        private int width;
+
+        public MapSquare this[int x, int y]
+        {
+            get { return map[(y * width) + x]; }
+        }
+
+        public MapIndexer(MapSquare[] map, int width)
+        {
+            this.map = map;
+            this.width = width;
+        }
+    }
+
+    private MapIndexer mapIndex;
+    public MapIndexer Map
+    {
+        get { return mapIndex; }
+    }
+
+    public Chunk(int width, int height, NeighbourSpec[] neighbourSpecs, TileSpec[] tileSpecs)
+    {
+        this.width = width;
+        this.height = height;
+        this.neighbourSpecs = neighbourSpecs;
+        this.tileSpecs = tileSpecs;
+        this.map = new MapSquare[width * height];
+        this.mapIndex = new MapIndexer(map, width);
+
+        // Initialise the map with all possible combinations of tile
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                map[(y * width) + x] = new MapSquare(x, y, neighbourSpecs.Select(n => n.Index));
+            }
+        }
+    }
+
+    public void BuildTilemap()
+    {
+        int loopLimit = height * width;
+
+        while (map.Any(s => s.Possibilities.Count > 1) && loopLimit-- > 0)
+        {
+            // Select the square on the map with the least possible tile options
+            var square = map
+                .Where(s => s.Possibilities.Count > 1)
+                .OrderBy(s => s.Possibilities.Count)
+                .First();
+
+            // Assign a random, legal tile to that square
+            int selected = SelectWeightedRandomTile(square);
+
+            square.SelectTile(selected);
+
+            // Collapse probabilities on neighbour squares
+            Collapse(map, square);
+
+            // Repeat!
+        }
+    }
+
+    private int SelectWeightedRandomTile(MapSquare square)
+    {
+        // Add some noise to the values so we get a random sort order and break ties between items with the same weighting in a random way
+        float noiseWeight = 0.01f;
+        var probabilities = square.Possibilities.Select(p => tileSpecs[p].Weight + (r.NextDouble() * noiseWeight));
+
+        // Order by priority, biggest first
+        var choices = probabilities.Zip(square.Possibilities, (a, b) => (P: a, I: b)).OrderByDescending(c => c.P);
+        
+        double max = choices.Sum(c => c.P);
+        double cutoff = r.NextDouble() * max;
+        double sum = 0;
+
+        // Walk down the list until we pass the randomly selected cutoff.  Return that index.
+        foreach (var c in choices)
+        {
+            sum += c.P;
+            if (sum > cutoff)
+                return c.I;
+        }
+
+        // Must be the last item...
+        return choices.Last().I;
+    }
+
+    private void Collapse(MapSquare[] map, MapSquare square)
+    {
+        List<MapSquare> recurse = new List<MapSquare>();
+
+        // Right
+        if (square.x < width - 1)
+        {
+            var other = map[square.y * width + square.x + 1];
+            var allowed = neighbourSpecs.Where(s => square.Possibilities.Contains(s.Index)).SelectMany(x => x.AllowedRight).Distinct();
+            if(other.Restrict(allowed))
+                recurse.Add(other);
+        }
+
+        // Bottom
+        if (square.y > 0)
+        {
+            var other = map[(square.y - 1) * width + square.x];
+            var allowed = neighbourSpecs.Where(s => square.Possibilities.Contains(s.Index)).SelectMany(x => x.AllowedBelow).Distinct();
+            if(other.Restrict(allowed))
+                recurse.Add(other);
+        }
+
+        // Left
+        if (square.x > 0)
+        {
+            var other = map[square.y * width + square.x - 1];
+            var allowed = neighbourSpecs.Where(s => square.Possibilities.Contains(s.Index)).SelectMany(x => x.AllowedLeft).Distinct();
+            if(other.Restrict(allowed))
+                recurse.Add(other);
+        }
+
+        // Top
+        if (square.y < height - 1)
+        {
+            var other = map[(square.y + 1) * width + square.x];
+            var allowed = neighbourSpecs.Where(s => square.Possibilities.Contains(s.Index)).SelectMany(x => x.AllowedAbove).Distinct();
+            if(other.Restrict(allowed))
+                recurse.Add(other);
+        }
+
+        recurse.ForEach(s => Collapse(map, s));
     }
 }
 
@@ -134,67 +268,18 @@ public class GridDecorator : MonoBehaviour
         {
             if(!chunks.ContainsKey(vector))
             {
-                Chunk chunk = InitialiseChunk();
+                Chunk chunk = new Chunk(chunkWidth, chunkHeight, neighbourSpecs, tileSpecs);
                 chunks.Add(vector, chunk);
 
                 // Copy edges from existing neighbour
 
                 var t = Time.realtimeSinceStartup;
-                BuildTilemap(chunk);
+                chunk.BuildTilemap();
                 print($"Wave function collapse completed in {(Time.realtimeSinceStartup - t).ToString("f6")}");
 
                 DrawChunk(chunk, vector);
             }
         }
-    }
- 
-    private void BuildTilemap(Chunk chunk)
-    {
-        int loopLimit = chunkHeight * chunkWidth;
-
-        while (chunk.map.Any(s => s.Possibilities.Count > 1) && loopLimit-- > 0)
-        {
-            // Select the square on the map with the least possible tile options
-            var square = chunk.map
-                .Where(s => s.Possibilities.Count > 1)
-                .OrderBy(s => s.Possibilities.Count)
-                .First();
-
-            // Assign a random, legal tile to that square
-            int selected = SelectWeightedRandomTile(square);
-
-            square.SelectTile(selected);
-
-            // Collapse probabilities on neighbour squares
-            Collapse(chunk.map, square);
-
-            // Repeat!
-        }
-    }
-
-    private int SelectWeightedRandomTile(MapSquare square)
-    {
-        // Add some noise to the values so we get a random sort order and break ties between items with the same weighting in a random way
-        float noiseWeight = 0.01f;
-        var probabilities = square.Possibilities.Select(p => tileSpecs[p].Weight + (r.NextDouble() * noiseWeight));
-
-        // Order by priority, biggest first
-        var choices = probabilities.Zip(square.Possibilities, (a, b) => (P: a, I: b)).OrderByDescending(c => c.P);
-        
-        double max = choices.Sum(c => c.P);
-        double cutoff = r.NextDouble() * max;
-        double sum = 0;
-
-        // Walk down the list until we pass the randomly selected cutoff.  Return that index.
-        foreach (var c in choices)
-        {
-            sum += c.P;
-            if (sum > cutoff)
-                return c.I;
-        }
-
-        // Must be the last item...
-        return choices.Last().I;
     }
 
     private void DrawChunk(Chunk chunk, Vector2Int origin)
@@ -203,7 +288,7 @@ public class GridDecorator : MonoBehaviour
         {
             for (int x = 0; x < chunkWidth; x++)
             {
-                int tileIndex = chunk.map[(y * chunkWidth) + x].TileIndex;
+                int tileIndex = chunk.Map[x, y].TileIndex;
                 var spec = tileSpecs[tileIndex];
 
                 int drawX = origin.x + x;
@@ -218,64 +303,5 @@ public class GridDecorator : MonoBehaviour
                 }
             }
         }
-    }
-
-    private Chunk InitialiseChunk()
-    {
-        var map = new MapSquare[chunkWidth * chunkHeight];
-
-        // Initialise the map with all possible combinations of tile
-        for (int x = 0; x < chunkWidth; x++)
-        {
-            for (int y = 0; y < chunkHeight; y++)
-            {
-                map[(y * chunkWidth) + x] = new MapSquare(x, y, neighbourSpecs.Select(n => n.Index));
-            }
-        }
-
-        return new Chunk(map);
-    }
-
-    private void Collapse(MapSquare[] map, MapSquare square)
-    {
-        List<MapSquare> recurse = new List<MapSquare>();
-
-        // Right
-        if (square.x < chunkWidth - 1)
-        {
-            var other = map[square.y * chunkWidth + square.x + 1];
-            var allowed = neighbourSpecs.Where(s => square.Possibilities.Contains(s.Index)).SelectMany(x => x.AllowedRight).Distinct();
-            if(other.Restrict(allowed))
-                recurse.Add(other);
-        }
-
-        // Bottom
-        if (square.y > 0)
-        {
-            var other = map[(square.y - 1) * chunkHeight + square.x];
-            var allowed = neighbourSpecs.Where(s => square.Possibilities.Contains(s.Index)).SelectMany(x => x.AllowedBelow).Distinct();
-            if(other.Restrict(allowed))
-                recurse.Add(other);
-        }
-
-        // Left
-        if (square.x > 0)
-        {
-            var other = map[square.y * chunkWidth + square.x - 1];
-            var allowed = neighbourSpecs.Where(s => square.Possibilities.Contains(s.Index)).SelectMany(x => x.AllowedLeft).Distinct();
-            if(other.Restrict(allowed))
-                recurse.Add(other);
-        }
-
-        // Top
-        if (square.y < chunkHeight - 1)
-        {
-            var other = map[(square.y + 1) * chunkWidth + square.x];
-            var allowed = neighbourSpecs.Where(s => square.Possibilities.Contains(s.Index)).SelectMany(x => x.AllowedAbove).Distinct();
-            if(other.Restrict(allowed))
-                recurse.Add(other);
-        }
-
-        recurse.ForEach(s => Collapse(map, s));
     }
 }
